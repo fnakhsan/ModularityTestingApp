c95d1696173a3fde2a0c25bfb4f125bb04882b25
fnakhsan <fn.akhsan@gmail.com>
2022-11-02 18:42:48 +0700

    Not Fixed yet

--- ModularityTestingApp/Form1.Designer.cs
+++ ModularityTestingApp/Form1.Designer.cs
@@ -44,6 +44,8 @@
             this.textBoxClass = new System.Windows.Forms.TextBox();
             this.textBoxTotalOut = new System.Windows.Forms.TextBox();
             this.labelClass = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBoxIn.SuspendLayout();
             this.groupBoxOut.SuspendLayout();
@@ -54,9 +56,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(0, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.Size = new System.Drawing.Size(141, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Browse your .CS file";
+            this.label1.Text = "Browse your project";
             this.label1.UseWaitCursor = true;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
@@ -64,9 +66,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(0, 51);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(113, 29);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Choose File";
+            this.button1.Text = "Choose Folder";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.UseWaitCursor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -86,11 +88,13 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(160, 80);
+            this.groupBox1.Size = new System.Drawing.Size(160, 159);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Choose File";
@@ -212,6 +216,28 @@
             this.labelClass.TabIndex = 6;
             this.labelClass.Text = "Class";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Choose File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseWaitCursor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Browse your c# file";
+            this.label2.UseWaitCursor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -251,5 +277,7 @@
         private TextBox textBoxClass;
         private TextBox textBoxTotalOut;
         private Label labelClass;
+        private Label label2;
+        private Button button2;
     }
 }
--- ModularityTestingApp/Form1.cs
+++ ModularityTestingApp/Form1.cs
@@ -24,50 +24,10 @@ namespace ModularityTestingApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int stringExtends = 0;
-            int stringNew = 0;
-            int stringClass = 0;
-            string? line;
-
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "C Sharp Files (.cs)|*.cs";
-            openFileDialog.Multiselect = true;
-
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
-                foreach (string file in openFileDialog.FileNames)
-                {
-                    try
-                    {
-                        StreamReader sr = new StreamReader(file);
-                        StringBuilder sb = new StringBuilder();
-
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            if (Regex.IsMatch(line, @"\s:\s")) stringExtends++;
-                            if (Regex.IsMatch(line, @"\snew\s")) stringNew++;
-                            if (Regex.IsMatch(line, @"\sclass\s")) stringClass++;
-                            sb.AppendLine(line);
-                        }
-
-                        sr.Close();
-
-                        richTextBox1.Text = sb.ToString();
-
-                        textBoxExtend.Text = stringExtends.ToString();
-                        textBoxNew.Text = stringNew.ToString();
-                        textBoxClass.Text = stringClass.ToString();
-
-                        textBoxTotalOut.Text = (stringClass).ToString();
-                        textBoxTotalIn.Text = (stringExtends + stringNew).ToString();
-
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-
-                }
+                
             }
         }
 
@@ -107,6 +67,58 @@ namespace ModularityTestingApp
         }
 
         private void labelTotalIn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int stringExtends = 0;
+            int stringNew = 0;
+            int stringClass = 0;
+            string? line;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "C Sharp Files (.cs)|*.cs";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = openFileDialog.FileName;
+                try
+                {
+                    StreamReader sr = new StreamReader(file);
+                    StringBuilder sb = new StringBuilder();
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (Regex.IsMatch(line, @"\s:\s")) stringExtends++;
+                        if (Regex.IsMatch(line, @"\snew\s")) stringNew++;
+                        if (Regex.IsMatch(line, @"\sclass\s")) stringClass++;
+                        sb.AppendLine(line);
+                    }
+
+                    sr.Close();
+
+                    richTextBox1.Text = sb.ToString();
+
+                    textBoxExtend.Text = stringExtends.ToString();
+                    textBoxNew.Text = stringNew.ToString();
+                    textBoxClass.Text = stringClass.ToString();
+
+                    textBoxTotalOut.Text = (stringClass).ToString();
+                    textBoxTotalIn.Text = (stringExtends + stringNew).ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+
+        private void label2_Click_1(object sender, EventArgs e)
         {
 
         }
